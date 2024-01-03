c35c19ce1f0ef63cee8bc1196646c0b70adaf877
WCY-dt <834421194@qq.com>
2024-01-03 22:50:32 +0800

    basic works

---
+++ README.md
@@ -0,0 +1,50 @@
+# WebGL-Universe
+
+一个使用原生 WebGL 制作的太阳系动态模型。模型包含了太阳、八大行星及月球。
+
+各星球大小、轨道半径、自转和公转周期等数据均来自维基百科，贴图来自 [planet-texture-maps](https://planet-texture-maps.fandom.com/wiki/Planet_Texture_Maps_Wiki)。
+
+这个项目也是图形学课程的结课作业。
+
+- 项目地址：[github.com/WCY-dt/WebGL-Universe](https://github.com/WCY-dt/WebGL-Universe)
+- 项目 Demo：[ch3nyang.top/WebGL-Universe](https://ch3nyang.top/WebGL-Universe)
+
+## 使用方法
+
+- 你可以克隆本项目
+
+  ```shell
+  git clone
+  ```
+
+  然后打开 `index.html`。
+
+- 我也提供了托管在 Github Pages 上的[站点](https://ch3nyang.top/WebGL-Universe)。
+
+进入后，使用鼠标拖动旋转，使用鼠标滚轮缩放。
+
+`main.js` 提供了一些可调选项，包括：
+
+- 球体精度
+- 行星比例
+- 太阳比例
+- 轨道比例
+- 自转与公转速度
+- 默认相机位置
+
+## 技术细节
+
+项目使用了教程 [webgl2fundamentals](https://webgl2fundamentals.org/) 提供的 [twgl](https://twgljs.org/)，省去 WebGL 中大量重复的代码，如缓冲区绑定、节点位置设置等，但保留了 WebGL 中的所有关键流程。
+
+主要技术细节如下：
+
+- **光照**：在太阳中心处设置点光源提供光照。考虑到现实中星球间隔很远，所以不需要处理阴影的问题。
+- **特殊处理太阳**：太阳本身是发光的，因此向在片段着色器传入一个全局变量 `u_isSun` 用于标记是否是太阳。如果是太阳，则不计算光照，直接贴上纹理。
+- **场景图**：月球绕着地球转，而地球绕着太阳转，这里使用了具有层级结构的场景图来实现这一点。项目将星球、轨道全部放置在场景图下，后期添加星球更加方便。
+- **轨迹球**：使用轨迹球实现鼠标拖动。
+
+## TODO
+
+- **材质**：将星球的材质修改得更趋近于现实
+- **天空盒**：添加宇宙背景
+- **可调选项**：增加滑动控件，直接在页面上调整可调选项
---
+++ main.js
@@ -0,0 +1,405 @@
+"use strict";
+
+const spherePrecision = 30;
+const planetScale = 0.00005;
+const sunScale = 0.000001;
+const orbitScale = 0.5;
+const rotationSpeed = 0.01;
+const cameraDistance = 2000;
+
+var vs = `#version 300 es
+
+in vec4 a_position;
+in vec3 a_normal;
+in vec2 a_texcoord;
+
+uniform vec3 u_lightWorldPosition;
+uniform vec3 u_viewWorldPosition;
+uniform mat4 u_world;
+uniform mat4 u_worldViewProjection;
+uniform mat4 u_worldInverseTranspose;
+
+out vec4 v_position;
+out vec3 v_normal;
+out vec2 v_texcoord;
+out vec3 v_surfaceToLight;
+out vec3 v_surfaceToView;
+
+void main() {
+    gl_Position = u_worldViewProjection * a_position;
+    v_position = normalize(gl_Position);
+
+    v_texcoord = a_texcoord;
+
+    v_normal = mat3(u_worldInverseTranspose) * a_normal;
+    vec3 surfaceWorldPosition = (u_world * a_position).xyz;
+    v_surfaceToLight = u_lightWorldPosition - surfaceWorldPosition;
+    v_surfaceToView = u_viewWorldPosition - surfaceWorldPosition;
+}
+`;
+
+var fs = `#version 300 es
+
+precision highp float;
+
+in vec4 v_position;
+in vec3 v_normal;
+in vec2 v_texcoord;
+in vec3 v_surfaceToLight;
+in vec3 v_surfaceToView;
+
+uniform sampler2D u_texture;
+uniform float u_shininess;
+uniform vec3 u_lightColor;
+uniform vec3 u_specularColor;
+
+uniform bool u_isSun;
+
+out vec4 outColor;
+
+void main() {
+
+    vec3 normal = normalize(v_normal);
+
+    vec3 surfaceToLightDirection = normalize(v_surfaceToLight);
+    vec3 surfaceToViewDirection = normalize(v_surfaceToView);
+    vec3 halfVector = normalize(surfaceToLightDirection + surfaceToViewDirection);
+
+    float light = dot(normal, surfaceToLightDirection);
+    float specular = 0.0;
+    if (light > 0.0) {
+        specular = pow(dot(normal, halfVector), u_shininess);
+    }
+
+    outColor = texture(u_texture, v_texcoord);
+
+    if (!u_isSun) { 
+        outColor.rgb *= light * u_lightColor;
+        outColor.rgb += specular * u_specularColor;
+    }
+}
+`;
+
+var Node = function () {
+    this.children = [];
+    this.localMatrix = m4.identity();
+    this.worldMatrix = m4.identity();
+};
+
+Node.prototype.setParent = function (parent) {
+    if (this.parent) {
+        var ndx = this.parent.children.indexOf(this);
+        if (ndx >= 0) {
+            this.parent.children.splice(ndx, 1);
+        }
+    }
+    if (parent) {
+        parent.children.push(this);
+    }
+    this.parent = parent;
+};
+
+Node.prototype.updateWorldMatrix = function (matrix) {
+    if (matrix) {
+        m4.multiply(matrix, this.localMatrix, this.worldMatrix);
+    } else {
+        m4.copy(this.localMatrix, this.worldMatrix);
+    }
+    var worldMatrix = this.worldMatrix;
+    this.children.forEach(function (child) {
+        child.updateWorldMatrix(worldMatrix);
+    });
+};
+
+function main() {
+    /* Initialize WebGL */
+    // Get A WebGL context
+    var canvas = document.querySelector("#canvas");
+    var gl = canvas.getContext("webgl2");
+    if (!gl) {
+        return;
+    }
+    twgl.setAttributePrefix("a_");
+
+    // Initialize sphere
+    var sphereBufferInfo = flattenedPrimitives.createSphereBufferInfo(
+        gl,
+        10,
+        spherePrecision,
+        Math.floor(spherePrecision / 2)
+    );
+
+    // setup GLSL program
+    var programInfo = twgl.createProgramInfo(gl, [vs, fs]);
+    var sphereVAO = twgl.createVAOFromBufferInfo(gl, programInfo, sphereBufferInfo);
+    var fieldOfViewRadians = degToRad(60);
+
+    /* Initialize nodes */
+    // orbit and radius
+    var solarSystemNode = new Node();
+    var mercuryOrbitNode = new Node();
+    mercuryOrbitNode.localMatrix = m4.translation(58 * orbitScale, 0, 0);
+    var venusOrbitNode = new Node();
+    venusOrbitNode.localMatrix = m4.translation(108 * orbitScale, 0, 0);
+    var earthOrbitNode = new Node();
+    earthOrbitNode.localMatrix = m4.translation(149 * orbitScale, 0, 0);
+    var moonOrbitNode = new Node();
+    moonOrbitNode.localMatrix = m4.translation(30 * orbitScale, 0, 0);
+    var marsOrbitNode = new Node();
+    marsOrbitNode.localMatrix = m4.translation(227 * orbitScale, 0, 0);
+    var jupiterOrbitNode = new Node();
+    jupiterOrbitNode.localMatrix = m4.translation(778 * orbitScale, 0, 0);
+    var saturnOrbitNode = new Node();
+    saturnOrbitNode.localMatrix = m4.translation(1426 * orbitScale, 0, 0);
+    var uranusOrbitNode = new Node();
+    uranusOrbitNode.localMatrix = m4.translation(2870 * orbitScale, 0, 0);
+    var neptuneOrbitNode = new Node();
+    neptuneOrbitNode.localMatrix = m4.translation(4498 * orbitScale, 0, 0);
+    // planet
+    var sunNode = generateNode(
+        696000 * sunScale,
+        true,
+        "./public/Sun.webp"
+    );
+    var mercuryNode = generateNode(
+        2439 * planetScale,
+        false,
+        "./public/Mercury.webp"
+    );
+    var venusNode = generateNode(
+        6052 * planetScale,
+        false,
+        "./public/Venus.webp"
+    );
+    var earthNode = generateNode(
+        6371 * planetScale,
+        false,
+        "./public/Earth.webp"
+    );
+    var marsNode = generateNode(
+        3389 * planetScale,
+        false,
+        "./public/Mars.webp"
+    );
+    var jupiterNode = generateNode(
+        69911 * planetScale,
+        false,
+        "./public/Jupiter.webp"
+    );
+    var saturnNode = generateNode(
+        58232 * planetScale,
+        false,
+        "./public/Saturn.webp"
+    );
+    var uranusNode = generateNode(
+        25362 * planetScale,
+        false,
+        "./public/Uranus.webp"
+    );
+    var neptuneNode = generateNode(
+        24622 * planetScale,
+        false,
+        "./public/Neptune.webp"
+    );
+    var moonNode = generateNode(
+        1737 * planetScale,
+        false,
+        "./public/Moon.webp"
+    );
+    // trackball
+    var trackballNode = new Node();
+    trackballNode.localMatrix = m4.translation(0, cameraDistance, 0);
+
+    /* hierarchy */
+    sunNode.setParent(solarSystemNode);
+    mercuryOrbitNode.setParent(solarSystemNode);
+    mercuryNode.setParent(mercuryOrbitNode);
+    venusOrbitNode.setParent(solarSystemNode);
+    venusNode.setParent(venusOrbitNode);
+    earthOrbitNode.setParent(solarSystemNode);
+    earthNode.setParent(earthOrbitNode);
+    moonOrbitNode.setParent(earthOrbitNode);
+    moonNode.setParent(moonOrbitNode);
+    marsOrbitNode.setParent(solarSystemNode);
+    marsNode.setParent(marsOrbitNode);
+    jupiterOrbitNode.setParent(solarSystemNode);
+    jupiterNode.setParent(jupiterOrbitNode);
+    saturnOrbitNode.setParent(solarSystemNode);
+    saturnNode.setParent(saturnOrbitNode);
+    uranusOrbitNode.setParent(solarSystemNode);
+    uranusNode.setParent(uranusOrbitNode);
+    neptuneOrbitNode.setParent(solarSystemNode);
+    neptuneNode.setParent(neptuneOrbitNode);
+    trackballNode.setParent(solarSystemNode);
+
+    /* trackball */
+    trackballControl();
+
+    /* draw */
+    var objects = [
+        sunNode,
+        mercuryNode,
+        venusNode,
+        earthNode,
+        moonNode,
+        marsNode,
+        jupiterNode,
+        saturnNode,
+        uranusNode,
+        neptuneNode,
+    ];
+    var objectsToDraw = [
+        sunNode.drawInfo,
+        mercuryNode.drawInfo,
+        venusNode.drawInfo,
+        earthNode.drawInfo,
+        moonNode.drawInfo,
+        marsNode.drawInfo,
+        jupiterNode.drawInfo,
+        saturnNode.drawInfo,
+        uranusNode.drawInfo,
+        neptuneNode.drawInfo,
+    ];
+    requestAnimationFrame(drawScene);
+
+    /* functions */
+
+    function degToRad(d) {
+        return d * Math.PI / 180;
+    }
+
+    function generateNode(scale, isSun, textureSrc) {
+        var planetNode = new Node();
+        planetNode.localMatrix = m4.scaling(scale, scale, scale);
+        planetNode.drawInfo = {
+            uniforms: {
+                u_shininess: 150,
+                u_lightColor: m4.normalize([1, 1, 1]),
+                u_specularColor: m4.normalize([1, 1, 1]),
+                u_lightWorldPosition: [0, 0, 0],
+                u_isSun: isSun,
+            },
+            programInfo: programInfo,
+            bufferInfo: sphereBufferInfo,
+            vertexArray: sphereVAO,
+        };
+        
+        var img = new Image();
+        img.onload = function () {
+            const texture = twgl.createTexture(gl, {
+                src: img
+            });
+            planetNode.drawInfo.uniforms.u_texture = texture;
+        };
+        img.src = textureSrc;
+
+        return planetNode;
+    }
+
+    function drawScene(time) {
+        time *= 0.001;
+
+        twgl.resizeCanvasToDisplaySize(gl.canvas);
+
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        gl.enable(gl.CULL_FACE);
+        gl.enable(gl.DEPTH_TEST);
+
+        gl.clearColor(0, 0, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+        var projectionMatrix =
+            m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
+
+        var cameraPosition = trackballNode.worldMatrix.slice(12, 15);
+        var target = [0, 0, 0];
+        var up = [0, 0, 1];
+        var cameraMatrix = m4.lookAt(cameraPosition, target, up);
+
+        var viewMatrix = m4.inverse(cameraMatrix);
+
+        var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
+
+        // orbit
+        m4.multiply(m4.yRotation(100 / 87.97 * rotationSpeed), mercuryOrbitNode.localMatrix, mercuryOrbitNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 224.7 * rotationSpeed), venusOrbitNode.localMatrix, venusOrbitNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 365.24 * rotationSpeed), earthOrbitNode.localMatrix, earthOrbitNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 686.98 * rotationSpeed), marsOrbitNode.localMatrix, marsOrbitNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 11.8565 / 365 * rotationSpeed), jupiterOrbitNode.localMatrix, jupiterOrbitNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 29.448 / 365 * rotationSpeed), saturnOrbitNode.localMatrix, saturnOrbitNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 84.02 / 365 * rotationSpeed), uranusOrbitNode.localMatrix, uranusOrbitNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 164.79 / 365 * rotationSpeed), neptuneOrbitNode.localMatrix, neptuneOrbitNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 27.32 * rotationSpeed), moonOrbitNode.localMatrix, moonOrbitNode.localMatrix);
+        // spin
+        m4.multiply(m4.yRotation(100 / 58.646 * rotationSpeed), mercuryNode.localMatrix, mercuryNode.localMatrix);
+        m4.multiply(m4.yRotation(-100 / 243.02 * rotationSpeed), venusNode.localMatrix, venusNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 0.997 * rotationSpeed), earthNode.localMatrix, earthNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 1.026 * rotationSpeed), marsNode.localMatrix, marsNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 0.410 * rotationSpeed), jupiterNode.localMatrix, jupiterNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 0.426 * rotationSpeed), saturnNode.localMatrix, saturnNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 0.718 * rotationSpeed), uranusNode.localMatrix, uranusNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 0.671 * rotationSpeed), neptuneNode.localMatrix, neptuneNode.localMatrix);
+        m4.multiply(m4.yRotation(100 / 27.32 * rotationSpeed), moonNode.localMatrix, moonNode.localMatrix);
+
+        solarSystemNode.updateWorldMatrix();
+
+        objects.forEach(function (object) {
+            var worldViewProjectionMatrix = m4.multiply(viewProjectionMatrix, object.worldMatrix);
+            var worldInverseMatrix = m4.inverse(object.worldMatrix);
+            var worldInverseTransposeMatrix = m4.transpose(worldInverseMatrix);
+
+            object.drawInfo.uniforms.u_world = object.worldMatrix;
+            object.drawInfo.uniforms.u_worldViewProjection = worldViewProjectionMatrix;
+            object.drawInfo.uniforms.u_worldInverseTranspose = worldInverseTransposeMatrix;
+
+            object.drawInfo.uniforms.u_viewWorldPosition = cameraPosition;
+        });
+
+        twgl.drawObjectList(gl, objectsToDraw);
+
+        requestAnimationFrame(drawScene);
+    }
+
+    function trackballControl() {
+        var mouseDown = false;
+        var lastMouseX = null;
+        var lastMouseY = null;
+
+        canvas.onmousedown = function (e) {
+            mouseDown = true;
+            lastMouseX = e.clientX;
+            lastMouseY = e.clientY;
+        };
+
+        canvas.onmouseup = function (e) {
+            mouseDown = false;
+        };
+
+        canvas.onmousemove = function (e) {
+            if (!mouseDown) {
+                return;
+            }
+            var newX = e.clientX;
+            var newY = e.clientY;
+
+            var deltaX = newX - lastMouseX;
+            var deltaY = newY - lastMouseY;
+
+            var newRotationMatrix = m4.multiply(m4.xRotation(deltaY * 0.01), m4.yRotation(deltaX * 0.01));
+            trackballNode.localMatrix = m4.multiply(newRotationMatrix, trackballNode.localMatrix);
+
+            lastMouseX = newX;
+            lastMouseY = newY;
+        };
+
+        canvas.onwheel = function (e) {
+            var scale = 1 + e.deltaY * 0.001;
+            var newScaleMatrix = m4.scaling(scale, scale, scale);
+            trackballNode.localMatrix = m4.multiply(newScaleMatrix, trackballNode.localMatrix);
+        };
+    }
+}
+
+main();
